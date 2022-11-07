19ba7ee5c338a90895c5e5ae468ee0cb8e35548a
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-11-07 18:41:01 +0100

    modifing the dbcontext

--- eBooks/Data/AppDbContext.cs
+++ eBooks/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eBooks.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eBooks.Data
 {
@@ -8,5 +9,24 @@ namespace eBooks.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author_Book>().HasKey(am => new
+            {
+                am.AuthorId,
+                am.BookId
+            });
+            modelBuilder.Entity<Author_Book>().HasOne(m => m.Book).WithMany(am => am.Author_Books).HasForeignKey(m => m.BookId);
+            modelBuilder.Entity<Author_Book>().HasOne(m => m.Author).WithMany(am => am.Author_Books).HasForeignKey(m => m.AuthorId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Author_Book> Author_Books { get; set; }
+        public DbSet<BookStore> BookStores { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
     }
 }
